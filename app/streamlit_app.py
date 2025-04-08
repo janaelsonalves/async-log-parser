edf665f4fe54ef6d7eca44705acafb41a55b0e3c
Janaelson Alves <jannaelson@hotmail.com>
2025-04-08 18:15:37 -0300

    Deployment

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from streamlit_dynamic_filters import DynamicFilters
 import asyncio
 import aiofiles
 import tempfile
@@ -9,10 +10,13 @@ import altair as alt
 from io import StringIO
 import nest_asyncio
 
+
+
 patterns = {
-            'target_lines': re.compile(r'(Logged users|User Authenticated)'),
+            'target_lines': re.compile(r'Radius Accounting'),
             'datetime': re.compile(r'(\d{4}-\d{2}-\d{2}\s+\d{2}:\d{2}:\d{2})'),
             'key_value': re.compile(r'([A-Za-z]+\.[A-Za-z-]+)=([^,\s\[\]]+)')
+            # 'key_value': re.compile(r'(\w[\w.-]*)=([^,]+)')
         }
 
 # ------------------------------ CONFIG ------------------------------
@@ -23,18 +27,19 @@ LOG_PATTERN = re.compile(r'(\w[\w.-]*)=([^,]+)')
 CHUNK_SIZE = 100
 
 # -------------------------- PARSING LOGIC ---------------------------
-
 async def process_lines(lines, filename):
     parsed = []
     for line in lines:
         if patterns['target_lines'].search(line):        
+        # if re.search(r'10.65', line):
             log_entry = {}
             # Extrai data e hora
-            if dt_match := patterns['datetime'].search(line):
-                log_entry['Timestamp'] = dt_match.group(1)
+            dt_match = patterns['datetime'].search(line)
+            log_entry['RADIUS.Timestamp'] = dt_match.group(1)
 
             # Extrai todos os pares chave=valor
-            matches = LOG_PATTERN.findall(line)  # Aplica regex na linha
+            # matches = LOG_PATTERN.findall(line)
+            matches = patterns['key_value'].findall(line)  # Aplica regex na linha
             if matches:
                 log_entry["RADIUS.Filename"] = os.path.basename(filename)  # Nome do arquivo
                 for key, value in matches:
@@ -60,68 +65,93 @@ async def process_file_async(path, filename, chunk_size=CHUNK_SIZE):
     results = await asyncio.gather(*tasks)
 
     all_logs = []
+
     for parsed in results:
-        all_logs.extend(parsed)
+        all_logs.extend(parsed) 
 
     return all_logs
 
 def run_batch_processing(filepaths_and_names):
     tasks = [
-        process_file_async(path, name)
-        for path, name in filepaths_and_names
+        process_file_async(path, name) for path, name in filepaths_and_names
     ]
     
     nest_asyncio.apply()
     return asyncio.run(asyncio.gather(*tasks))
 
-# ---------------------------- UI APP -------------------------------
+@st.cache_data
+def prepare_dataframe(df_logs):
+    try:        
+        df_logs = df_logs.sort_values(['RADIUS.Timestamp', 'RADIUS.Acct-Username'], ascending=False)
+        df_logs = df_logs.drop_duplicates(subset='RADIUS.Acct-Username', keep='first')
+        df_logs = df_logs[df_logs['RADIUS.Acct-NAS-IP-Address'].str.contains('10.65')]
+    except Exception as e:
+        print(e)
+    return df_logs
 
-st.set_page_config(page_title="Async Log Parser", layout="wide")
-st.title("📄 Async Log File Parser")
-
-with st.expander("Clique para abrir e carregar arquivos", expanded=True):
-    uploaded_files = st.file_uploader(
-        "Upload one or more .log or .txt files", type=["log", "txt"], accept_multiple_files=True
-    )
-
-if uploaded_files:
+def create_tempfiles(uploaded_files):
     temp_files = []
     for uploaded_file in uploaded_files:
         with tempfile.NamedTemporaryFile(delete=False, mode='wb') as tmp:
             tmp.write(uploaded_file.read())
             temp_files.append((tmp.name, uploaded_file.name))
+    return temp_files
 
-    if st.button("🚀 Process Uploaded Files"):
-        with st.spinner("Processing asynchronously..."):
-            results = run_batch_processing(temp_files)
+def remove_tempfiles(temp_files):
+    for path, _ in temp_files:
+        os.remove(path)
 
-        all_logs = []
-        for (logs), (_, filename) in zip(results, temp_files):
-            all_logs.extend(logs)
+def extend_tempfiles_list(temp_files, results):
+    all_logs = []
+    # for (logs), (_, filename) in zip(results, temp_files):
+    for logs, _ in zip(results, temp_files):
+        all_logs.extend(logs)
+    return all_logs
 
+# ---------------------------- UI APP -------------------------------
+st.set_page_config(page_title="Analisador de logs do CPPM (Radius)", layout="wide")
+st.title("📄 Analisador de logs do CPPM (Radius)")
+
+with st.expander("## Carregue arquivos em formato .log ou .txt", expanded=True, icon="📋") as exp:
+    uploaded_files = st.file_uploader("Upload log files", type=["log", "txt"], accept_multiple_files=True, label_visibility='hidden')
+
+if uploaded_files:
+    temp_files = create_tempfiles(uploaded_files)
+
+    if st.button("🚀 Excluir arquivos temporários"):
         # Cleanup temp files
-        for path, _ in temp_files:
-            os.remove(path)
+        # remove_tempfiles(temp_files)
+        # st.file_uploader = []
+        pass
 
+    disabled = False
+    if st.button("🚀 Processasr arquivos carregados", disabled=False, key="active"):
+        with st.spinner("Processando arquivos... Aguarde!"):
+            from datetime import datetime
+            inicial = datetime.now()
+            results = run_batch_processing(temp_files)
+            final = datetime.now()
+            disabled = True
+
+        st.write(f"**Tempo de processamento:** _{final - inicial}_")
+
+        all_logs = extend_tempfiles_list(temp_files, results)
+        
         # Logs DataFrame
         df_logs = pd.DataFrame(all_logs)
+        # df_logs = prepare_dataframe(df_logs)
+        df_logs = df_logs.sort_values(['RADIUS.Timestamp', 'RADIUS.Acct-Username'], ascending=False)
+        df_logs = df_logs.drop_duplicates(subset='RADIUS.Acct-Username', keep='first')
+        df_logs = df_logs[df_logs['RADIUS.Acct-NAS-IP-Address'].str.contains('10.65')]
 
-        df_logs = df_logs.sort_values('Common.Username', ascending=False)
-
-        df_logs = df_logs.drop_duplicates(subset='Common.Username', keep='first')
+        remove_tempfiles(temp_files)
 
         # Table
         st.subheader(f"🧾 Registros processados ({len(df_logs)})")
-        st.dataframe(df_logs)
 
-        # Chart
-        # st.subheader("📈 Logs Over Time")
-        # chart = alt.Chart(df_logs).mark_line().encode(
-        #     x='timestamp:T',
-        #     y='count():Q',
-        #     color='level:N'
-        # ).interactive()
-        # st.altair_chart(chart, use_container_width=True)
+        if len(df_logs):
+            st.dataframe(df_logs[['RADIUS.Filename', 'RADIUS.Timestamp', 'RADIUS.Acct-Username', 'RADIUS.Acct-NAS-IP-Address', 'RADIUS.Acct-Framed-IP-Address', 'RADIUS.Acct-NAS-Port-Type']]) 
+            # st.dataframe(df_logs)
 
         # Export Button
         st.subheader("📥 Export Logs as CSV")
